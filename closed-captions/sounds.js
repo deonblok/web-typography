53d61f7fa08de434cc3fc4a3532dad0654190905
Deon <deon.blok@hva.nl>
2022-10-26 00:58:42 +0200

    laatste puntjes op de i

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,30 +1,33 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-    // trillen
+    // trillen 1
     0,
-    // rood
+    // rood 2
     9,
-    // scale
+    // boxshadow 3
     10,
-    // paars
-    15,
-    // goud
-    17,
-    // groengeen
-    24,
-    // magenta
-    26.8,
-    // pink
-    31,
-    // zalm
-    32,
-    // papaya
-    34,
-    // blueviolet
-    40,
-    // olijf
-    45,
-
-    50,
+    // paars 4
+    16.2,
+    // goud 5
+    17.2,
+    // zwart 6
+    25.5,
+    26.2,
+    // paars       16->17 paars
+    30.2,
+    // magenta 7    15->17 zwart naar wit 
+    76,
+    // pink 8
+    79,
+    // // zalm 9
+    // 55,
+    // // papaya 10
+    // 60,
+    // // blueviolet 11
+    // 85,
+    // // olijf 12
+    // 86,
+    // // 13
+    // 50,
 ];
